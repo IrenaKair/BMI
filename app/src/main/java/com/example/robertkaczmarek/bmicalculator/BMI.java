7c48006fb8717605231f017049161f02c8539ada
Robert <kaczmarekrob13@gmail.com>
2017-03-04 19:25:34 +0100

    nowy

--- app/src/main/java/com/example/robertkaczmarek/bmicalculator/BMI.java
+++ app/src/main/java/com/example/robertkaczmarek/bmicalculator/BMI.java
@@ -24,46 +24,60 @@ public class BMI extends AppCompatActivity {
         String h = height.getText().toString();
         Double hh = Double.parseDouble(h);
 
-        Double r = ww/(hh*hh);
+        Double r = ww/(hh*hh/10000);
         String a = String.format("%.2f", r);
 
-        Double maxR = (hh*hh)*24.99;
+        Double maxR = (hh*hh/10000)*24.99;
         String max = String.format("%.2f", maxR);
 
-        Double minR = (hh*hh)*18;
+        Double minR = (hh*hh/10000)*18;
         String min = String.format("%.2f", minR);
 
         TextView result = (TextView)findViewById(R.id.result);
-        result.setText("Twoje BMI: "+ "\n" + a);
+        String bmi = getString(R.string.bmi);
+        result.setText(bmi + "\n" + a);
         TextView comment = (TextView)findViewById(R.id.comment);
+        String fitA = getString(R.string.fitA);
+        String fitB = getString(R.string.fitB);
+        String fitC = getString(R.string.fitC);
+        String fitD = getString(R.string.fitD);
+        String fatE = getString(R.string.fatE);
+        String fatG = getString(R.string.fatG);
+        String fatF = getString(R.string.fatF);
+        String fatH = getString(R.string.fatH);
 
-        String norma = view.getTag().toString();
+        //String norma = view.getTag().toString();
         if (r < 15) {
-            comment.setText("Wygłodzenie "+"\n"+ "Dla ciebie " +"\n" +"min waga to "+"\n"+ min +"kg");
+            comment.setText(fitA);// +"\n"+ "Dla ciebie " +"\n" +"min waga to "+"\n"+ min +"kg");
 
         } else if (r < 17 && r >= 15) {
-            comment.setText("Wychudzenie"+"\n"+ "Dla ciebie " +"\n" +"min waga to "+"\n"+ min +"kg");
+            comment.setText(fitB);// +"\n"+ "Dla ciebie " +"\n" +"min waga to "+"\n"+ min +"kg");
 
         }else if (r < 18 && r >= 17) {
-            comment.setText("Niedowada"+"\n"+ "Dla ciebie " +"\n" +"min waga to "+"\n"+ min +"kg");
+            comment.setText(fitC);//+"\n"+ "Dla ciebie " +"\n" +"min waga to "+"\n"+ min +"kg");
 
         }
         else if (r < 25 && r >= 18) {
-            comment.setText(norma);
+            comment.setText(fitD);
 
         }else if (r < 30 && r >= 25) {
-            comment.setText("Masz nadwagę" +"\n"+ "Dla ciebie " +"\n" +"max waga to "+"\n"+ max +"kg");
+            comment.setText(fatE);// +"\n"+ "Dla ciebie " +"\n" +"max waga to "+"\n"+ max +"kg");
             
 
         }else if (r < 35 && r >= 30) {
-            comment.setText("Otyłość I stopnia"+"\n"+ "Dla ciebie " +"\n" +"max waga to "+"\n"+ max +"kg");
+            comment.setText(fatF);//+"\n"+ "Dla ciebie " +"\n" +"max waga to "+"\n"+ max +"kg");
 
         }else if (r < 40 && r >= 35) {
-            comment.setText("Otyłość II stopnia"+"\n"+ "Dla ciebie " +"\n" +"max waga to "+"\n"+ max +"kg");
+            comment.setText(fatG);// +"\n"+ "Dla ciebie " +"\n" +"max waga to "+"\n"+ max +"kg");
 
         }else if (r >=40)  {
-            comment.setText("Otyłość skrajna "+"\n"+ "Dla ciebie " +"\n" +"max waga to "+"\n"+ max +"kg");
+            comment.setText(fatH);// +"\n"+ "Dla ciebie " +"\n" +"max waga to "+"\n"+ max +"kg");
         }
+        TextView comment2 = (TextView) findViewById(R.id.comment2);
+        String ooo = getString(R.string.oooo);
+        String aa = getString(R.string.aa);
+        String kg = getString(R.string.kg);
+        comment2.setText(ooo +"\n"+ min +" "+ aa+" "+ max+" "+kg);
     }
 
     public void cleare(View view) {
@@ -73,9 +87,11 @@ public class BMI extends AppCompatActivity {
 
         TextView result = (TextView)findViewById(R.id.result);
         TextView comment = (TextView) findViewById(R.id.comment);
+        TextView comment2 = (TextView) findViewById(R.id.comment2);
         weight.setText("");
         height.setText("");
         result.setText("");
         comment.setText("");
+        comment2.setText("");
     }
 }
