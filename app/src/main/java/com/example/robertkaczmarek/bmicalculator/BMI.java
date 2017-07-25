6f02b68b7079fffe6568f97952a448456c0f6950
Igor Napierala <igornapierala@gmail.com>
2017-07-25 22:12:36 +0000

    BMI.java edited online with Bitbucket

--- app/src/main/java/com/example/robertkaczmarek/bmicalculator/BMI.java
+++ app/src/main/java/com/example/robertkaczmarek/bmicalculator/BMI.java
@@ -8,13 +8,10 @@ import android.view.Window;
 import android.widget.EditText;
 
 public class BMI extends AppCompatActivity {
-
     Double result2;
     Double minR;
     Double maxR;
     String fit;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,38 +19,28 @@ public class BMI extends AppCompatActivity {
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_bmi);
-
-
-
     }
-
 
     public void calculate(View view) {
         EditText weight = (EditText) findViewById(R.id.weight);
         String w = weight.getText().toString();
-      //  Double ww = Double.parseDouble(w);
+//      Double ww = Double.parseDouble(w);
         EditText height = (EditText)findViewById(R.id.height);
         String h = height.getText().toString();
 
-
         Intent go = new Intent(BMI.this, result1.class);
+        
         go.putExtra(result1.WEIGHT, w);
         go.putExtra(result1.HEIGHT, h);
 
         startActivity(go);
     }
 
-
-
-
     public void cleare(View view) {
         EditText weight = (EditText) findViewById(R.id.weight);
-
         EditText height = (EditText)findViewById(R.id.height);
-
 
         weight.setText("");
         height.setText("");
-
     }
 }
